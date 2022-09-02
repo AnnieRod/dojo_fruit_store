9805c673dba6bb73fd112834ad0bbbf40becbd38
AnnieRod <euphie.13@gmail.com>
2022-09-02 17:53:58 -0500

    opcional tienda frutas

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)  
 
@@ -5,10 +7,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+##Solicitud de info para poner en HTML de check-out
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
+    apple = request.form ['apple']
+    strawberry = request.form['strawberry']
+    blackberry = request.form['blackberry']
+    raspberry = request.form['raspberry']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    student = request.form['student_id']
+    total = int(apple)+ int(strawberry) + int(blackberry) + int(raspberry)  ##suma total de productos
+    today_date = datetime.now()
+    date = today_date.strftime("%m/%d/%Y, %H:%M")
+    print(f"Cobrando a {first_name} {last_name} por {total} frutas")
+    return render_template("checkout.html", apple = apple, strawberry = strawberry, blackberry = blackberry, raspberry = raspberry, first_name = first_name, last_name = last_name, student = student, total = total, date = date)
 
 @app.route('/fruits')         
 def fruits():
